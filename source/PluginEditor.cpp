e823473cbb6243777361d1dd4a526843d2294dc9
Tomasz Woźniak <tw.blaken@gmail.com>
2015-04-21 15:19:35 +0200

    removed head profile view/display

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -1,25 +1,17 @@
 #include "PluginEditor.h"
 
+
 HrtfBiAuralAudioProcessorEditor::HrtfBiAuralAudioProcessorEditor(HrtfBiAuralAudioProcessor& p)
 	:
 	AudioProcessorEditor(&p),
 	processor(p),
+	mainDisplay(new MainDisplay(this)),
 	bgColor(51, 51, 51),
 	fgColor(73, 166, 201),
-	topViewX(15.f),
-	topViewY(60.f),
-	sideViewX(240.f),
-	sideViewY(250.f),
-	azimuth(0.),
-	elevation(0.),
 	nextHrtf("next", 0.f, fgColor),
 	prevHrtf("prev", 0.5f, fgColor)
 {
 	setSize(350, 420);
-
-	topViewImage = ImageFileFormat::loadFrom(head_top_png, head_top_png_size);
-	sideViewImage = ImageFileFormat::loadFrom(head_side_png, head_side_png_size);
-	sourceImage = ImageFileFormat::loadFrom(source_icon_png, source_icon_png_size);
 
 	elevationSlider.setSliderStyle(Slider::LinearVertical);
 	elevationSlider.setRange(-90, 90, 0.1);
@@ -27,6 +19,7 @@ HrtfBiAuralAudioProcessorEditor::HrtfBiAuralAudioProcessorEditor(HrtfBiAuralAudi
 	elevationSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 50, 20);
 	elevationSlider.addListener(this);
 	elevationSlider.setName("Elevation");
+	elevationSlider.addListener(mainDisplay);
 	addAndMakeVisible(elevationSlider);
 
 	crossoverSlider.setSliderStyle(Slider::LinearHorizontal);
@@ -59,6 +52,8 @@ HrtfBiAuralAudioProcessorEditor::HrtfBiAuralAudioProcessorEditor(HrtfBiAuralAudi
 	addAndMakeVisible(nextHrtf);
 	nextHrtf.addListener(this);
 	addAndMakeVisible(prevHrtf);
+
+	addAndMakeVisible(mainDisplay);
 }
 
 HrtfBiAuralAudioProcessorEditor::~HrtfBiAuralAudioProcessorEditor()
@@ -69,53 +64,7 @@ void HrtfBiAuralAudioProcessorEditor::paint(Graphics& g)
 {
 	g.setColour(bgColor);
 	g.fillAll();
-	drawDisplays(g);
-	drawGridLines(g);
-	drawSources(g);
 	drawBordersAndLabels(g);
-}
-
-void HrtfBiAuralAudioProcessorEditor::drawGridLines(Graphics& g)
-{
-	auto w = topViewImage.getWidth();
-	auto h = topViewImage.getHeight();
-	g.setColour(Colours::white);
-	g.setOpacity(0.5f);
-	auto lineHorizontal = Line<float>(topViewX, topViewY + h * 0.5f, topViewX + w, topViewY + h * 0.5f);
-	auto lineVertical = Line<float>(topViewX + w * 0.5f, topViewY, topViewX + w * 0.5f, topViewY + h);
-	float dashes [] = { 3, 2 };
-	g.drawDashedLine(lineHorizontal, dashes, 2);
-	g.drawDashedLine(lineVertical, dashes, 2);
-	w = sideViewImage.getWidth();
-	h = sideViewImage.getHeight();
-	lineHorizontal = Line<float>(sideViewX, sideViewY + h * 0.5f, sideViewX + w, sideViewY + h * 0.5f);
-	lineVertical = Line<float>(sideViewX + w * 0.5f, sideViewY, sideViewX + w * 0.5f, sideViewY + h);
-	g.drawDashedLine(lineHorizontal, dashes, 2);
-	g.drawDashedLine(lineVertical, dashes, 2);
-}
-
-void HrtfBiAuralAudioProcessorEditor::drawDisplays(Graphics& g)
-{
-	g.drawImageAt(topViewImage, topViewX, topViewY);
-	g.drawImageAt(sideViewImage, sideViewX, sideViewY);
-	g.setColour(bgColor);
-	g.drawEllipse(topViewX, topViewY, topViewImage.getWidth(), topViewImage.getHeight(), 2.0f);
-	g.drawEllipse(sideViewX, sideViewY, sideViewImage.getWidth(), sideViewImage.getHeight(), 2.0f);
-}
-
-void HrtfBiAuralAudioProcessorEditor::drawSources(Graphics& g)
-{
-	auto w = topViewImage.getWidth();
-	auto h = topViewImage.getHeight();
-	auto radius = w * 0.5f - 30;
-	auto x = radius * std::sin(azimuth);
-	auto y = radius * std::cos(azimuth);
-	auto sourceX = topViewX + w * 0.5f + x;
-	auto sourceY = topViewY + h * 0.5f - y;
-	ColourGradient grad(fgColor, sourceX, sourceY, Colours::transparentBlack, topViewX + w * 0.5f, topViewY + h * 0.5f, true);
-	g.setGradientFill(grad);
-	g.fillEllipse(topViewX, topViewY, w, h);
-	g.drawImageAt(sourceImage, sourceX - sourceImage.getWidth() * 0.5f, sourceY - sourceImage.getHeight() * 0.5f);
 }
 
 void HrtfBiAuralAudioProcessorEditor::drawBordersAndLabels(Graphics& g)
@@ -141,23 +90,10 @@ void HrtfBiAuralAudioProcessorEditor::resized()
 {
 	prevHrtf.setBounds(100, 25, 40, 20);
 	nextHrtf.setBounds(220, 25, 40, 20);
-	elevationSlider.setBounds(280, 80, 50, 160);
+	mainDisplay->setBounds(20, 70, 260, 260);
+	elevationSlider.setBounds(280, 80, 50, 260);
 	crossoverSlider.setBounds(10, getBottom() - 50, 250, 50);
 	bypassButton.setBounds(getWidth() - 90, getBottom() - 40, 90, 30);
-}
-
-void HrtfBiAuralAudioProcessorEditor::mouseDrag(const MouseEvent &event)
-{
-	auto pos = event.getPosition();
-	if (pos.x > topViewX && pos.x < topViewX + topViewImage.getWidth() &&
-		pos.y > topViewY && pos.y < topViewY + topViewImage.getHeight())
-	{
-		auto x = pos.x - topViewX - topViewImage.getWidth() * 0.5f;
-		auto y = pos.y - topViewY - topViewImage.getHeight() * 0.5f;
-		azimuth = std::atan2(x, -y);
-		updateHrir();
-		repaint();
-	}
 }
 
 void HrtfBiAuralAudioProcessorEditor::sliderValueChanged(Slider* slider)
@@ -165,12 +101,6 @@ void HrtfBiAuralAudioProcessorEditor::sliderValueChanged(Slider* slider)
 	if (slider->getName() == crossoverSlider.getName())
 	{
 		processor.crossover.set(processor.crossover.fs, slider->getValue());
-	}
-	else
-	{
-		elevation = deg2rad(slider->getValue());
-		updateHrir();
-		repaint();
 	}
 }
 
@@ -186,15 +116,4 @@ void HrtfBiAuralAudioProcessorEditor::buttonClicked(Button* button)
 
 	processor.reset();
 	repaint();
-}
-
-void HrtfBiAuralAudioProcessorEditor::updateHrir()
-{
-	auto x = cos(elevation) * sin(azimuth);
-	auto y = cos(elevation) * cos(azimuth);
-	auto z = sin(elevation);
-
-	auto point = cartesianToInteraural(Point3Cartesian<double>(x, y, z));
-
-	processor.updateHRTF(rad2deg(point.azimuth), rad2deg(point.elevation));
 }
--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -1,55 +1,39 @@
 #pragma once
 
-#include <cmath>
 #include "PluginProcessor.h"
+#include "MainDisplay.h"
 #include "Helpers.h"
-#include "images/head_top.h"
-#include "images/head_side.h"
-#include "images/source_icon.h"
 #include "images/bypass_down.h"
 #include "images/bypass_up.h"
 
 
-class HrtfBiAuralAudioProcessorEditor : public AudioProcessorEditor,
-	private Slider::Listener,
-	private Button::Listener
+class HrtfBiAuralAudioProcessorEditor :
+	public AudioProcessorEditor,
+	public Slider::Listener,
+	public Button::Listener
 {
+	friend class MainDisplay;
 public:
-	HrtfBiAuralAudioProcessorEditor(HrtfBiAuralAudioProcessor&);
+	HrtfBiAuralAudioProcessorEditor(HrtfBiAuralAudioProcessor& processor);
 	~HrtfBiAuralAudioProcessorEditor();
 
-	void paint(Graphics&) override;
+	void paint(Graphics& g) override;
 	void resized() override;
-	void mouseDrag(const MouseEvent&) override;
-	void sliderValueChanged(Slider*) override;
+	void sliderValueChanged(Slider* slider) override;
 	void buttonClicked(Button* button) override;
 
 private:
-	void drawGridLines(Graphics& g);
-	void drawDisplays(Graphics& g);
-	void drawSources(Graphics& g);
 	void drawBordersAndLabels(Graphics& g);
-
-	void updateHrir();
 
 	HrtfBiAuralAudioProcessor& processor;
 	Colour bgColor;
 	Colour fgColor;
+	ScopedPointer<MainDisplay> mainDisplay;
 	ArrowButton nextHrtf;
 	ArrowButton prevHrtf;
 	Slider elevationSlider;
 	Slider crossoverSlider;
 	ImageButton bypassButton;
-	Image sourceImage;
-	Image topViewImage;
-	Image sideViewImage;
-
-	float topViewX;
-	float topViewY;
-	float sideViewX;
-	float sideViewY;
-	double azimuth;
-	double elevation;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(HrtfBiAuralAudioProcessorEditor)
 };
